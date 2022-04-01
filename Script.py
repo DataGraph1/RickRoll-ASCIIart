790e0ee8e8e80e018e9c4e0cdbab8912824b45d0
DataGraph <86309105+DataGraph1@users.noreply.github.com>
2022-04-01 16:44:33 +0100

    Add files via upload

--- Script.py
+++ Script.py
@@ -1,86 +1,5 @@
 import time
 
-stats = [] #player stats, filled from save file
-
-effects = [
-    "levitation",
-    "speed",
-    "danger",
-    "luck",
-    "hop"
-]
-
-itemCount = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    11,
-    12,
-    13
-]
-
-
-items = [ #all items in game
-    "club",
-    "dagger",
-    "short sword",
-    "sword",
-    "long sword",
-    "small axe",
-    "axe",
-    "large axe",
-    "small hammer",
-    "hammer",
-    "mace",
-    "short bow",
-    "bow",
-    "long bow",
-    "light crossbow",
-    "crossbow",
-    "large crossbow",
-    "glass vial",
-    "leather cap",
-    "leather tunic",
-    "leather leggings",
-    "small sheild",
-    "sheild",
-    "large sheild",
-    "net",
-    "woodern arrows",
-    "stone arrows",
-    "copper arrows",
-    "steal arrows",
-    "enchanted arrows",
-]
-
-def slotSelector(): #user chooses save file
-    for x in range(1,4): #displays save files
-        f = open("Saves/"+str(x)+".txt")
-        print(str(x) + "]", f.readline(1) + "%")
-        f.close
-    
-    option = input("Choose a save slot") #allows save file choice
-    if "1" in option:
-        f = open("Saves/1.txt")
-    elif "2" in option:
-        f = open("Saves/2.txt")
-    elif "3" in option:
-        f = open("Saves/3.txt")
-    else:
-        print("Non-existent save file, try again!")
-        slotSelector()
-
-    for x in range(2,5): #save file stats into list
-        stats.append(f.readlines(27))
-    f.close
-
 def screenSetUp(): #makes sure terminal is big enough for ASCII art
     print("1" * 125)
     for x in range(1, 50):
@@ -92,8 +11,8 @@ def animation(): #plays ASCII art animation
     lyricNum = 1
     while True:
         for frameNum in range(1,51):
-            frame = open(".config/SaveFiles/"+str(frameNum)+".txt", "r")
-            lyric = open(".config/SaveData/"+str(lyricNum)+".txt", "r")
+            frame = open("Frames/"+str(frameNum)+".txt", "r")
+            lyric = open("Lyrics/"+str(lyricNum)+".txt", "r")
             print(frame.read())
             print(lyric.read())
             frame.close
