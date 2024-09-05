95a70af4fbb62b341592349ff63f8081284407a5
unseeyou <unseeyoubestmod@gmail.com>
2024-09-05 19:28:58 +1000

    fix: refactor to use snake case and open files with context manager

--- Script.py
+++ Script.py
@@ -1,6 +1,6 @@
 import time
 
-def screenSetUp(): #makes sure terminal is big enough for ASCII art
+def screen_setup(): #makes sure terminal is big enough for ASCII art
     print("1" * 125)
     for x in range(1, 50):
         print("")
@@ -8,19 +8,17 @@ def screenSetUp(): #makes sure terminal is big enough for ASCII art
     animation()
 
 def animation(): #plays ASCII art animation
-    lyricNum = 1
+    lyric_num = 1
     while True:
         for frameNum in range(1,51):
-            frame = open("Frames/"+str(frameNum)+".txt", "r")
-            lyric = open("Lyrics/"+str(lyricNum)+".txt", "r")
-            print(frame.read())
-            print(lyric.read())
-            frame.close
-            lyric.close
+            with open("Frames/"+str(frameNum)+".txt") as frame:
+                print(frame.read())
+            with open("Lyrics/"+str(lyric_num)+".txt") as lyric:
+                print(lyric.read())
             time.sleep(0.0417)
-        if lyricNum < 7:
-            lyricNum += 1
+        if lyric_num < 7:
+            lyric_num += 1
         else:
-            lyricNum = 1
+            lyric_num = 1
 
-screenSetUp()
+screen_setup()
