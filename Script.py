6032592648639a283fe514fa75a24825ea6c1550
DataGraph <86309105+DataGraph1@users.noreply.github.com>
2024-10-02 19:37:24 +0100

    Updated some of the code formatting

--- Script.py
+++ Script.py
@@ -1,24 +1,34 @@
 import time
 
 def screen_setup(): #makes sure terminal is big enough for ASCII art
-    print("1" * 125)
-    for x in range(1, 50):
-        print("")
-    x = input("Resize terminal so: \n 1. You can see this entire message\n 2. You can see the numbers above\n 3. All those numbers are on the same line\nThen press Enter")
+    print('0' * 125)
+    for _ in range(1, 50):
+        print('')
+
+    _ = input('''
+Resize terminal so:
+    1. You can see this entire message
+    2. You can see the numbers above
+    3. All those numbers are on the same line
+Then press Enter
+''')
+    
     animation()
 
 def animation(): #plays ASCII art animation
-    lyric_num = 1
+    lyricNum = 1
     while True:
         for frameNum in range(1,51):
-            with open("Frames/"+str(frameNum)+".txt") as frame:
+            with open('Frames/'+str(frameNum)+'.txt') as frame:
                 print(frame.read())
-            with open("Lyrics/"+str(lyric_num)+".txt") as lyric:
+            with open('Lyrics/'+str(lyricNum)+'.txt') as lyric:
                 print(lyric.read())
+
             time.sleep(0.0417)
-        if lyric_num < 7:
-            lyric_num += 1
+
+        if lyricNum < 7:
+            lyricNum += 1
         else:
-            lyric_num = 1
+            lyricNum = 1
 
 screen_setup()
